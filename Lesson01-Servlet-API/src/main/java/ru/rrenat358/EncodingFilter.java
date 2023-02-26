015b2183360680368d802c8acef6b2e3a8eea5eb
rrenat358 <rrenat358@gmail.com>
2023-02-26 17:46:26 +0300

    1746

---
+++ Lesson01-Servlet-API/src/main/java/ru/rrenat358/EncodingFilter.java
@@ -0,0 +1,27 @@
+package ru.rrenat358;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/*")
+public class EncodingFilter implements Filter {
+
+    private FilterConfig filterConfig;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.filterConfig = filterConfig;
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        servletResponse.setContentType("text/html");
+        servletResponse.setCharacterEncoding("UTF-8");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}

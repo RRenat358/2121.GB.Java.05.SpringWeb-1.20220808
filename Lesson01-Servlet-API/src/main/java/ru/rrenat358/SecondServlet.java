d997f53d3e96924be33fd19848e3005626b7afb9
rrenat358 <rrenat358@gmail.com>
2023-02-26 16:34:51 +0300

    1634

---
+++ Lesson01-Servlet-API/src/main/java/ru/rrenat358/SecondServlet.java
@@ -0,0 +1,36 @@
+package ru.rrenat358;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/aaa")
+public class SecondServlet implements Servlet {
+
+    private ServletConfig servletConfig;
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        this.servletConfig = servletConfig;
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return servletConfig;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        servletResponse.getWriter().println("<h1>aaa</h1>");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}

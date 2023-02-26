5b442874c484a8a698ffa43c60f74630b82a93b3
rrenat358 <rrenat358@gmail.com>
2023-02-26 15:14:51 +0300

    1514

--- Lesson01-Servlet-API/src/main/java/ru/rrenat358/FirstServlet.java
+++ Lesson01-Servlet-API/src/main/java/ru/rrenat358/FirstServlet.java
@@ -6,14 +6,16 @@ import java.io.IOException;
 
 public class FirstServlet implements Servlet {
 
+    private ServletConfig servletConfig;
+
     @Override
     public void init(ServletConfig servletConfig) throws ServletException {
-
+        this.servletConfig = servletConfig;
     }
 
     @Override
     public ServletConfig getServletConfig() {
-        return null;
+        return servletConfig;
     }
 
     @Override

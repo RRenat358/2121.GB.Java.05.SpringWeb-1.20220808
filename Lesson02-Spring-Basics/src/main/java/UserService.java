731d3fcf9b65c861cdb4e257cc085b5f0af67fb5
rrenat358 <rrenat358@gmail.com>
2023-02-28 10:33:35 +0300

    1033

---
+++ Lesson02-Spring-Basics/src/main/java/UserService.java
@@ -0,0 +1,46 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import ru.titov.persist.User;
+import ru.titov.persist.UserRepository;
+
+import javax.annotation.PostConstruct;
+
+@Service
+public class UserService {
+
+/*
+    @Autowired
+    @Qualifier("first")
+*/
+    private UserRepository userRepository;
+
+/*
+    @PostConstruct
+    public void init() {
+        System.out.println("Метод postConstruct отработал");
+    }
+*/
+
+
+    public void insert(User user) {
+        if(user.getRole().equals("ADMIN") || user.getRole().equals("GUEST")) {
+            this.userRepository.insert(user);
+        } else {
+            throw new IllegalArgumentException("Incorrect role");
+        }
+    }
+
+    public int findAll() {
+        return this.userRepository.findAll().size();
+    }
+
+/*
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+*/
+
+
+}

594c1b905ab694c6b2bcd9db93fe900a2d299483
rrenat358 <rrenat358@gmail.com>
2023-03-08 13:04:33 +0300

    1304

--- Lesson05-Hibernate/src/main/java/ru/rrenat358/Main.java
+++ Lesson05-Hibernate/src/main/java/ru/rrenat358/Main.java
@@ -1,7 +1,10 @@
 package ru.rrenat358;
 
+import org.hibernate.cfg.Configuration;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        new Configuration();
     }
 }

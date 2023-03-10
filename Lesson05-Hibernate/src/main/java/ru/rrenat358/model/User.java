021bbc47f3f51f1c7680ec2ee67bb969bb013ff4
rrenat358 <rrenat358@gmail.com>
2023-03-10 16:39:59 +0300

    1639 контакты со списком ID

--- Lesson05-Hibernate/src/main/java/ru/rrenat358/model/User.java
+++ Lesson05-Hibernate/src/main/java/ru/rrenat358/model/User.java
@@ -1,19 +1,6 @@
 package ru.rrenat358.model;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Embedded;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.NamedQueries;
-import jakarta.persistence.NamedQuery;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -48,6 +35,10 @@ public class User {
 
     @Column(nullable = false, length = 1024)
     private String password;
+
+    // контакты со списком ID
+//    @ElementCollection
+//    private List<Long> ordersIds;
 
 //    @OneToOne(mappedBy = "user",
 //            cascade = {CascadeType.ALL},

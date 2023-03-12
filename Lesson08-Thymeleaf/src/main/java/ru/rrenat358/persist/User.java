54120c774b2891eb552cac5809d60b2522740134
rrenat358 <rrenat358@gmail.com>
2023-03-12 13:48:51 +0300

    1348

---
+++ Lesson08-Thymeleaf/src/main/java/ru/rrenat358/persist/User.java
@@ -0,0 +1,34 @@
+package ru.rrenat358.persist;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 1024)
+    private String password;
+
+    @Transient
+    private String matchingPassword;
+
+    public User(String username) {
+        this.username = username;
+    }
+}

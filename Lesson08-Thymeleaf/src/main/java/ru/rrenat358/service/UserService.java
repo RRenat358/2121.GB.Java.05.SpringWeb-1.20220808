3e2ec9132f49d45f69d63690a41aab349fbd13b8
rrenat358 <rrenat358@gmail.com>
2023-03-13 13:41:47 +0300

    1341 QUser.user

--- Lesson08-Thymeleaf/src/main/java/ru/rrenat358/service/UserService.java
+++ Lesson08-Thymeleaf/src/main/java/ru/rrenat358/service/UserService.java
@@ -4,13 +4,15 @@ package ru.rrenat358.service;
 import com.querydsl.core.BooleanBuilder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.rrenat358.model.User;
+import ru.rrenat358.model.QUser;
 import ru.rrenat358.model.dto.UserDto;
 import ru.rrenat358.model.mapper.UserDtoMapper;
 import ru.rrenat358.repository.UserRepository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Service
 @RequiredArgsConstructor
@@ -26,11 +28,26 @@ public class UserService {
 //    }
 
     public List<UserDto> findAllByFilter(String usernameFilter, String emailFilter) {
-//        QUser user = QUser.user;
-
+        QUser user = QUser.user;
         BooleanBuilder predicate = new BooleanBuilder();
 
-        return null;
+        if (usernameFilter != null || !usernameFilter.isBlank()) {
+            predicate.and(user.username.contains(usernameFilter.trim()));
+        }
+        if (emailFilter != null || !emailFilter.isBlank()) {
+            predicate.and(user.email.contains(emailFilter.trim()));
+        }
+        return StreamSupport.stream(userRepository.findAll(predicate).spliterator(), true)
+                .map(userFromDB -> {
+                    UserDto dto = new UserDto();
+
+                    dto.setId(userFromDB.getId());
+                    dto.setUsername(userFromDB.getUsername());
+                    dto.setEmail(userFromDB.getEmail());
+                    dto.setPassword(userFromDB.getPassword());
+
+                    return dto;
+                }).collect(Collectors.toList());
     }
 
 

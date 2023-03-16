83df83af57a991eee27c34269f740192a9e8efab
rrenat358 <rrenat358@gmail.com>
2023-03-16 13:57:13 +0300

    1357

--- Lesson10-Web/src/main/java/ru/rrenat358/lesson10web/Lesson10WebApplication.java
+++ Lesson10-Web/src/main/java/ru/rrenat358/lesson10web/Lesson10WebApplication.java
@@ -1,13 +1,23 @@
 package ru.rrenat358.lesson10web;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Lesson10WebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Lesson10WebApplication.class, args);
+        new SpringApplicationBuilder(Lesson10WebApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+
+
+
+
+
     }
 
 }

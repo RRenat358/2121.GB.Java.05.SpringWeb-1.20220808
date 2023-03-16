d4b93ce41b57cdcb82f05ea1d40b47d750fc7d5d
rrenat358 <rrenat358@gmail.com>
2023-03-16 13:49:57 +0300

    1349 Created module

---
+++ Lesson10-Web/src/main/java/ru/rrenat358/lesson10web/Lesson10WebApplication.java
@@ -0,0 +1,13 @@
+package ru.rrenat358.lesson10web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson10WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson10WebApplication.class, args);
+    }
+
+}
---
+++ Lesson10-Web/src/test/java/ru/rrenat358/lesson10web/Lesson10WebApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rrenat358.lesson10web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson10WebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

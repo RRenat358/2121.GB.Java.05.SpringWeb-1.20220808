c14d740feb9322f1bc78b751c631308bbde92ed0
rrenat358 <rrenat358@gmail.com>
2023-03-17 13:50:29 +0300

    1350

--- Lesson10-Frontend/src/app/app.module.ts
+++ Lesson10-Frontend/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import {RouterOutlet} from "@angular/router";
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    UserListComponent,
+    UserFormComponent
   ],
   imports: [
     BrowserModule,

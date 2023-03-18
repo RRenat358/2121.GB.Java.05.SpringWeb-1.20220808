5a42515d5128e0bf829eb26adff309f72a7e437e
rrenat358 <rrenat358@gmail.com>
2023-03-18 10:24:03 +0300

    1023 runnable, но не отображается листинг с пользователями

--- Lesson10-Frontend/src/app/app.module.ts
+++ Lesson10-Frontend/src/app/app.module.ts
@@ -1,14 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import {RouterOutlet} from "@angular/router";
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { UserListComponent } from './user-list/user-list.component';
-import { UserFormComponent } from './user-form/user-form.component';
-import { UserServiceComponent } from './user-service/user-service.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {UserListComponent} from './user-list/user-list.component';
+import {UserFormComponent} from './user-form/user-form.component';
+import {UserServiceComponent} from './user-service/user-service.component';
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 
@@ -30,4 +29,5 @@ import {HttpClientModule} from "@angular/common/http";
   providers: [UserServiceComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}

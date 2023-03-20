7233d6acf7a2db7953676be6dc0657897d955224
rrenat358 <rrenat358@gmail.com>
2023-03-20 20:06:19 +0300

    2005 add Lesson11-SpringApp

---
+++ Lesson11-SpringApp/src/main/java/ru/rrenat358/SpringApp11.java
@@ -0,0 +1,13 @@
+package ru.rrenat358;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApp11 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApp11.class, args);
+    }
+
+}
---
+++ Lesson11-SpringApp/src/main/java/ru/rrenat358/rest/UserResource.java
@@ -0,0 +1,54 @@
+package ru.rrenat358.rest;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.rrenat358.exceptions.EntityNotFoundException;
+import ru.rrenat358.model.dto.UserDto;
+import ru.rrenat358.service.UserService;
+
+import java.util.Optional;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
+public class UserResource {
+
+    private final UserService service;
+
+    @GetMapping
+    public Page<UserDto> listPage(
+            @RequestParam(required = false) String usernameFilter,
+            @RequestParam(required = false) String emailFilter,
+            @RequestParam(required = false) Optional<Integer> page,
+            @RequestParam(required = false) Optional<Integer> size,
+            @RequestParam(required = false) Optional<String> sortField
+    ) {
+        log.info("HERE WE ARE");
+        Integer pageValue = page.orElse(1) - 1;
+        Integer sizeValue = size.orElse(3);
+        String sortFieldValue = sortField.filter(s -> !s.isBlank()).orElse("id");
+        Page<UserDto> allByFilter = service.findAllByFilter(usernameFilter, emailFilter, pageValue, sizeValue, sortFieldValue);
+        return allByFilter;
+    }
+
+    @GetMapping("/{id}")
+    public UserDto form(@PathVariable("id") long id, Model model) {
+        UserDto userDto = service.findUserById(id).orElseThrow(() -> new EntityNotFoundException("User not found"));
+        return userDto;
+    }
+
+    @PostMapping
+    public UserDto saveUser(@RequestBody UserDto user) {
+        if (user.getId() != null) {
+            throw new IllegalArgumentException("Created user shouldn't have id");
+        }
+        service.save(user);
+        return user;
+    }
+
+
+}

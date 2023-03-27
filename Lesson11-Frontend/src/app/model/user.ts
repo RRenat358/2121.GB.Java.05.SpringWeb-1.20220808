c0ace240140cd5cfafb42e1b80f187e113abb9c3
rrenat358 <rrenat358@gmail.com>
2023-03-27 15:32:38 +0300

    1532

--- Lesson11-Frontend/src/app/model/user.ts
+++ Lesson11-Frontend/src/app/model/user.ts
@@ -1,8 +1,8 @@
 export class User {
 
-  constructor(public id:number | null,
-              public username:string,
-              public email:string,
-              public password:number) {
+    constructor(public id: number | null,
+                public username: string,
+                public email: string,
+                public password: string) {
   }
 }
--- Lesson11-Frontend/src/app/user-form/user-form.component.ts
+++ Lesson11-Frontend/src/app/user-form/user-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from "../model/user";
 
 @Component({
   selector: 'app-user-form',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-form.component.scss']
 })
 export class UserFormComponent implements OnInit {
+
+  user = new User(null, "", "", "");
+  error = false;
+  errorMessage = "";
+
 
   constructor() { }
 

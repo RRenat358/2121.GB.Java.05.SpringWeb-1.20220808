11b9d502f080a263b9ba40dc4b290c2dd65cf645
rrenat358 <rrenat358@gmail.com>
2023-03-28 23:10:50 +0300

    2310

--- Lesson12-SpringApp/src/main/java/ru/rrenat358/security/SecurityConfiguration.java
+++ Lesson12-SpringApp/src/main/java/ru/rrenat358/security/SecurityConfiguration.java
@@ -1,9 +1,16 @@
 package ru.rrenat358.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @EnableWebSecurity
 public class SecurityConfiguration {
@@ -26,6 +33,33 @@ public class SecurityConfiguration {
         authBuilder.userDetailsService(userDetailsService);
     }
 
+    @Configuration
+    public static class UiWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
 
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests()
+                    .antMatchers("/**/*.css", "/**/*.js").permitAll()
+                    .antMatchers("/").permitAll()
+                    .antMatchers("/user/**").hasAnyRole("ADMIN", "SUPER_ADMIN")
+                    .and()
+                    .formLogin()
+                    .successHandler((request, response, authentication) -> {
+                        Set<String> auths = authentication.getAuthorities().stream()
+                                .map(GrantedAuthority::getAuthority)
+                                .collect(Collectors.toSet());
+                        if (auths.contains("ROLE_ADMIN") || auths.contains("ROLE_SUPER_ADMIN")) {
+                            response.sendRedirect("/user");
+                        } else {
+                            response.sendRedirect("/");
+                        }
+                    })
+                    .and()
+                    .exceptionHandling()
+                    .accessDeniedPage("/access_denied");
+
+        }
+    }
 
 }

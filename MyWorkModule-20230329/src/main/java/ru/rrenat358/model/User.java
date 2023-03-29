2033a1c4b96c8429a0660f4bbd3c04b44ac29d19
rrenat358 <rrenat358@gmail.com>
2023-03-29 16:36:19 +0300

    1635 created

---
+++ MyWorkModule-20230329/src/main/java/ru/rrenat358/model/User.java
@@ -0,0 +1,38 @@
+package ru.rrenat358.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 1024)
+    private String password;
+
+    @ManyToMany
+    private Set<Role> roles;
+
+    public User(String username) {
+        this.username = username;
+    }
+}
---
+++ MyWorkModule-20230329/src/main/java/ru/rrenat358/model/dto/UserDto.java
@@ -0,0 +1,45 @@
+package ru.rrenat358.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import ru.rrenat358.model.Role;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+//Data Transfer Object
+
+    private Long id;
+
+    @NotBlank(message = "can not be empty!!!")
+    private String username;
+
+    @NotBlank
+    @Email
+    private String email;
+
+//    @Pattern(regexp = "^(?=.*?[0-9])(?=.*?[A-Z]).{8,}$", message = "Password too simple")
+    private String password;
+
+    @JsonIgnore
+    private String matchingPassword;
+
+    private Set<Role> roles;
+
+    public UserDto(Long id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+}
